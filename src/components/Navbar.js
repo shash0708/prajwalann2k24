07640ff7c011477a39227e4e9731a802a32a34d8
Shashank <97978650+shash0708@users.noreply.github.com>
2024-01-15 19:06:41 +0530

    okkblur

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,31 @@ import React, { Component } from 'react';
 import { Link } from 'react-scroll';
 import "./css/Navbar.css";
 import "../App.css";
-const PDF_FILE_URL = "r.pdf";
+const PDF_FILE_URL = "https://drive.google.com/drive/folders/18hcG1CnTTpnntdWiw6KQWiN7K1t93S3v?usp=sharing";
 
 class Navbar extends Component {
-  state = { clicked: false };
+  state = { 
+    clicked: false,
+    isScrolled: false,
+  };
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const scrollTop = window.scrollY;
+    const isScrolled = scrollTop > -10;
+    this.setState({ isScrolled });
+  };
+
+ openPDFInNewPage = () => {
+    window.open(PDF_FILE_URL, '_blank');
+  };
 
   handleClick = () => {
     this.setState({
@@ -23,8 +44,10 @@ class Navbar extends Component {
   };
 
   render() {
+    const { isScrolled } = this.state;
+
     return (
-      <div className="k">
+      <div className={`k ${isScrolled ? 'scrolled' : ''}`}>
         <nav>
           <div>
             {/* Use an <img> tag for your logo */}
@@ -74,7 +97,7 @@ class Navbar extends Component {
       smooth={true} 
       offset={-150} 
       duration={20}>Faq's</Link></li>
-            <li><Link href="/" onClick={() => this.handleDownloadPDF(PDF_FILE_URL)}>Resources</Link></li>
+            <li><Link href="/" onClick={() => this.openPDFInNewPage(PDF_FILE_URL)}>Resources</Link></li>
           </ul>
         </nav>
       </div>
