70cb2905d40e52cf8aa3565d98735b1b8227336a
Shashank <97978650+shash0708@users.noreply.github.com>
2024-01-19 20:38:32 +0530

    okkkk

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-scroll';
-import Timer from'./Timer'
+import Timer from './Timer';
 import "./css/Navbar.css";
 import "../App.css";
 const PDF_FILE_URL = "https://drive.google.com/drive/folders/18hcG1CnTTpnntdWiw6KQWiN7K1t93S3v?usp=sharing";
@@ -25,7 +25,7 @@ class Navbar extends Component {
     this.setState({ isScrolled });
   };
 
- openPDFInNewPage = () => {
+  openPDFInNewPage = () => {
     window.open(PDF_FILE_URL, '_blank');
   };
 
@@ -34,7 +34,8 @@ class Navbar extends Component {
       clicked: !this.state.clicked
     });
   };
- handleDownloadPDF = (url) => {
+
+  handleDownloadPDF = (url) => {
     const fileName = url.split("/").pop();
     const atag = document.createElement("a");
     atag.href = url;
@@ -45,80 +46,96 @@ class Navbar extends Component {
   };
 
   render() {
-    const { isScrolled } = this.state;
+    const { isScrolled, clicked } = this.state;
+    const isMobile = window.innerWidth <= 768; // Adjust the breakpoint as needed
 
     return (
       <div className={`k ${isScrolled ? 'scrolled' : ''}`}>
         <nav>
           <div>
             {/* Use an <img> tag for your logo */}
-            
             <h1>PRAJWALAN</h1>
-            
           </div>
           <div id="mobile" onClick={this.handleClick}>
-            {this.state.clicked ? (
+            {clicked ? (
               <i className="fa-sharp fa-solid fa-xmark"></i>
             ) : (
               <i className="fas fa-bars"></i>
             )}
           </div>
-          <ul id="navbar" className={this.state.clicked ? "active" : ""}>
-          <li> <Link 
-      activeClass="active" 
-      to="text" 
-      spy={true} 
-      smooth={true} 
-      offset={-500} 
-      duration={20} 
-    
-    >
-      Home
-    </Link></li>
-            <li><Link activeClass="active" 
-      to="About" 
-      spy={true} 
-      smooth={true} 
-      offset={-100} 
-      duration={20}>About</Link>
-      </li>
-            <li><Link activeClass="active" 
-      to="Tracks" 
-      spy={true} 
-      smooth={true} 
-      offset={-50} 
-      duration={20}>Tracks</Link></li>
-      
-      <li><Link activeClass="active" 
-      to="ca" 
-      spy={true} 
-      smooth={true} 
-      offset={-1400} 
-      duration={20}>Gallery</Link></li>
-
-
-            <li><Link activeClass="active" 
-      to="ca" 
-      spy={true} 
-      smooth={true} 
-      offset={-230} 
-      duration={20}>Prizes</Link></li>
-            <li><Link activeClass="active" 
-      to="t" 
-      spy={true} 
-      smooth={true} 
-      offset={-150} 
-      duration={20}>Faq's</Link></li>
+          <ul id="navbar" className={clicked ? "active" : ""}>
+            <li><Link 
+              activeClass="active" 
+              to="text" 
+              spy={true} 
+              smooth={true} 
+              offset={-500} 
+              duration={20} 
+            >
+              Home
+            </Link></li>
+            <li><Link 
+              activeClass="active" 
+              to="About" 
+              spy={true} 
+              smooth={true} 
+              offset={-100} 
+              duration={20}
+            >
+              About
+            </Link></li>
+            <li><Link 
+              activeClass="active" 
+              to="Tracks" 
+              spy={true} 
+              smooth={true} 
+              offset={-50} 
+              duration={20}
+            >
+              Tracks
+            </Link></li>
+            <li><Link 
+              activeClass="active" 
+              to="ca" 
+              spy={true} 
+              smooth={true} 
+              offset={isMobile ? -600 : -1500}  
+              duration={20}
+            >
+              Gallery
+            </Link></li>
+            <li><Link 
+              activeClass="active" 
+              to="ca" 
+              spy={true} 
+              smooth={true} 
+              offset={-230} 
+              duration={20}
+            >
+              Prizes
+            </Link></li>
+            <li><Link 
+              activeClass="active" 
+              to="t" 
+              spy={true} 
+              smooth={true} 
+              offset={-150} 
+              duration={20}
+            >
+              Faq's
+            </Link></li>
             <li><Link href="/" onClick={() => this.openPDFInNewPage(PDF_FILE_URL)}>Resources</Link></li>
-            <li><Link activeClass="active" 
-      to="t" 
-      spy={true} 
-      smooth={true} 
-      offset={500} 
-      duration={20}>Know us</Link></li>
+            <li><Link 
+              activeClass="active" 
+              to="t" 
+              spy={true} 
+              smooth={true} 
+              offset={500} 
+              duration={20}
+            >
+              Know us
+            </Link></li>
           </ul>
-          
-       
         </nav>
       </div>
     );
