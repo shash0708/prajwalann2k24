35518948cd151eaae34a8027590745102c796c78
Shashank <97978650+shash0708@users.noreply.github.com>
2024-01-23 11:04:58 +0530

    uhuh

--- src/components/Organizer.js
+++ src/components/Organizer.js
@@ -14,7 +14,7 @@ function Organizer() {
         <img src='assests/srkr.svg'  alt='/'/>
         <h2>Department of Computer science Engineering</h2>
         <h2>Sagi Ramakrishnam Raju Engineering College(A)</h2>
-        <p>Founded in 1980, Sagi Rama Krishnam Raju Engineering College (SRKR) is a beacon of excellence with a mission to empower rural students through technical education.<br></br> The Department of Computer Science and Engineering, established in 1991, started with a B.Tech. (CSE) program and 40 students, growing to an intake of 300 by 2021. The M.Tech. (CST) program began in 2006 with 18 students, increasing to 36 by 2011.
+        <p>Sagi Rama Krishnam Raju Engineering College (SRKR) established in 1980 ,is a beacon of excellence with a mission to empower rural students through technical education.<br></br> The Department of Computer Science and Engineering, established in 1991, started with a B.Tech. (CSE) program and 40 students, growing to an intake of 300 by 2021. The M.Tech. (CST) program began in 2006 with 18 students, increasing to 36 by 2011.
 </p></div>
           <div className='rr123'>
       <img src='assests/ace.svg'  alt='/'/>
