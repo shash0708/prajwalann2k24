21e00be860e9067a1c9dfaae6c5cd4517ec58841
Shashank <97978650+shash0708@users.noreply.github.com>
2024-07-28 22:02:24 +0530

    Update Loader.js

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -42,7 +42,7 @@ function Loader() {
         <>
           <Navbar />
           <Home />
-   <Poster/>
+   // <Poster/>
           {/* <Popup /> */}
         {/* <Timer1/> */}
             {/* <About1 /> */}
